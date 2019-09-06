24fbac1a5b3f35607a16de425852c7196927eb4f
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-09-06 19:31:27 +0300

    std::vector-based type indexer.

--- include/nttx/detail/type_indexer.hpp
+++ include/nttx/detail/type_indexer.hpp
@@ -3,7 +3,6 @@
 
 #include <nttx/detail/get_global_type_index.hpp>
 
-#include <boost/scope_exit.hpp>
 #include <boost/config.hpp>
 #include <boost/hana/type.hpp>
 
@@ -12,6 +11,7 @@
 #include <limits>
 #include <utility>
 #include <stdexcept>
+#include <vector>
 
 namespace nttx::detail {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -28,9 +28,7 @@ struct type_indexer
 
 private:
     static constexpr
-    auto invalid_index = std::numeric_limits<index_type>::max();
-
-    using allocator_traits = std::allocator_traits<allocator_type>;
+    auto const invalid_index = std::numeric_limits<index_type>::max();
 
 public:
     type_indexer()
@@ -38,186 +36,123 @@ public:
     = default;
 
     explicit
-    type_indexer(allocator_type allocator)
+    type_indexer(allocator_type const& allocator)
     noexcept
-    : allocator_(std::move(allocator))
+    : indices_(allocator)
     {}
 
-    type_indexer(type_indexer const& other, allocator_type allocator)
-    : allocator_(std::move(allocator))
-    , capacity_(other.capacity_)
-    , next_(other.next_)
+    type_indexer(type_indexer&& other, allocator_type const& allocator)
+    : indices_(std::move(other.indices_), allocator)
+    , next_index_(other.next_index_)
     {
-        auto locals2 = allocator_traits::allocate(allocator_, capacity_);
-        BOOST_SCOPE_EXIT_ALL(&) {
-            if (!locals2) { return; }
-            allocator_traits::deallocate(allocator_, locals2, capacity_);
-        };
-        std::uninitialized_copy_n(locals_, capacity_, locals2);
-        BOOST_SCOPE_EXIT_ALL(&) {
-            if (!locals2) { return; }
-            std::destroy_n(locals2, capacity_);
-        };
-        std::swap(locals_, locals2);
+        if (other.indices_.empty()) {
+            other.next_index_ = 0;
+        }
     }
-
-    type_indexer(type_indexer const& other)
-    : type_indexer(other, allocator_traits::select_on_container_copy_construction(other.allocator_))
-    {}
 
     type_indexer(type_indexer&& other)
     noexcept
-    : allocator_(other.allocator_)
+    : indices_(std::move(other.indices_))
+    , next_index_(std::exchange(other.next_index_, 0))
+    {}
+
+    type_indexer(type_indexer const& other, allocator_type const& allocator)
+    : indices_(other.indices_, allocator)
+    , next_index_(other.next_index_)
+    {}
+
+    type_indexer(type_indexer const& other)
+    = default;
+
+    auto& operator=(type_indexer&& other)
+    noexcept(noexcept(this->indices_ = std::move(other.indices_)))
     {
-        std::swap(locals_, other.locals_);
-        std::swap(capacity_, other.capacity_);
-        std::swap(next_, other.next_);
+        if (&other != this) {
+            indices_ = std::move(other.indices_);
+            next_index_ = other.next_index_;
+            if (other.indices_.empty()) {
+                other.next_index_ = 0;
+            }
+        }
+        return *this;
     }
 
     auto operator=(type_indexer const& other)
     -> type_indexer&
+    = default;
+
+    void swap(type_indexer& other)
+    noexcept(noexcept(std::swap(this->indices_, other.indices_)))
     {
-        if (&other != this) {
-            if (allocator_traits::propagate_on_container_copy_assignment::value
-             && !allocator_traits::is_always_equal::value && allocator_ != other.allocator_)
-            {
-                auto allocator2 = other.allocator_;
-                auto capacity2 = other.capacity_;
-                auto locals2 = allocator_traits::allocate(allocator2, capacity2);
-                BOOST_SCOPE_EXIT_ALL(&) {
-                    if (!locals2) { return; }
-                    allocator_traits::deallocate(allocator2, locals2, capacity2);
-                };
-                std::uninitialized_copy_n(other.locals_, other.capacity_, locals2);
-                BOOST_SCOPE_EXIT_ALL(&) {
-                    if (!locals2) { return; }
-                    std::destroy_n(locals2, capacity2);
-                };
-                { using std::swap; swap(allocator_, allocator2); }
-                std::swap(locals_, locals2);
-                std::swap(capacity_, capacity2);
-            } else {
-                auto capacity2 = other.capacity_;
-                auto locals2 = allocator_traits::allocate(allocator_, capacity2);
-                BOOST_SCOPE_EXIT_ALL(&) {
-                    if (!locals2) { return; }
-                    allocator_traits::deallocate(allocator_, locals2, capacity2);
-                };
-                std::uninitialized_copy_n(other.locals_, other.capacity_, locals2);
-                BOOST_SCOPE_EXIT_ALL(&) {
-                    if (!locals2) { return; }
-                    std::destroy_n(locals2, capacity2);
-                };
-                std::swap(locals_, locals2);
-                std::swap(capacity_, capacity2);
-            }
-            next_ = other.next_;
-        }
-        return *this;
-    }
-
-    auto operator=(type_indexer&& other)
-    -> type_indexer&
-    {
-        if (&other != this) {
-            //
-            //
-            // TODO
-            //
-            //
-
-            //if (allocator_ != other.allocator_ && )
-            //std::destroy_n(locals_, capacity_);
-            //allocator_traits::deallocate(allocator_, locals_, capacity_);
-
-            //if (allocator_traits::propagate_on_container_move_assignment::value
-            // && !allocator_traits::is_always_equal::value && allocator_ != other.allocator_)
-            //{
-
-            //    auto allocator2 = other.allocator_;
-            //    auto capacity2 = other.capacity_;
-            //    auto locals2 = allocator_traits::allocate(allocator2, capacity2);
-            //    BOOST_SCOPE_EXIT_ALL(&) {
-            //        if (!locals2) { return; }
-            //        allocator_traits::deallocate(allocator2, locals2, capacity2);
-            //    };
-            //    std::uninitialized_copy_n(other.locals_, other.capacity_, locals2);
-            //    BOOST_SCOPE_EXIT_ALL(&) {
-            //        if (!locals2) { return; }
-            //        std::destroy_n(locals2, capacity2);
-            //    };
-            //    { using std::swap; swap(allocator_, allocator2); }
-            //    std::swap(locals_, locals2);
-            //    std::swap(capacity_, capacity2);
-            //} else {
-            //}
-            next_ = other.next_;
-        }
-        return *this;
-    }
-
-    ~type_indexer() {
-        if (!locals_) { return; }
-        std::destroy_n(locals_, capacity_);
-        allocator_traits::deallocate(allocator_, locals_, capacity_);
+        std::swap(indices_, other.indices_);
+        std::swap(next_index_, other.next_index_);
     }
 
     auto get_allocator() const
     -> allocator_type
-    { return allocator_; }
+    {
+        return indices_.get_allocator();
+    }
 
     template <typename T>
     BOOST_FORCEINLINE
     auto operator()(boost::hana::basic_type<T> type)
     -> index_type
     {
-        auto global = get_global_type_index<domain_type>(type);
-        if (BOOST_UNLIKELY(global >= capacity_)) {
-            expand(global);
+        auto global_index = get_global_type_index<domain_type>(type);
+        if (BOOST_UNLIKELY(global_index >= indices_.size())) {
+            grow(global_index);
         }
-        auto local = locals_[global];
-        if (BOOST_UNLIKELY(local == invalid_index)) {
-            advance(global, local);
+        auto index = indices_[global_index];
+        if (BOOST_UNLIKELY(index == invalid_index)) {
+            index = occupy(global_index);
         }
-        return local;
+        return index;
+    }
+
+    auto limit() const
+    -> std::size_t
+    {
+        return next_index_;
+    }
+
+    void reset() {
+        indices_.clear();
+        next_index_ = 0;
+    }
+
+    void tighten() {
+        indices_.shrink_to_fit();
     }
 
 private:
-    [[no_unique_address]]
-    allocator_type allocator_;
-
-    index_type* locals_ = nullptr;
-    std::size_t capacity_ = 0;
-
-    std::size_t next_ = 0;
+    std::vector<index_type, allocator_type> indices_;
+    std::size_t next_index_ = 0;
 
     BOOST_NOINLINE
-    void expand(std::size_t global) {
-        auto capacity2 = global + 1;
-        auto locals2 = allocator_traits::allocate(allocator_, capacity2);
-        BOOST_SCOPE_EXIT_ALL(&) {
-            if (!locals2) { return; }
-            allocator_traits::deallocate(allocator_, locals2, capacity2);
-        };
-        auto init_end = std::uninitialized_copy_n(locals_, capacity_, locals2);
-        BOOST_SCOPE_EXIT_ALL(&) {
-            if (!locals2) { return; }
-            std::destroy(locals2, init_end);
-        };
-        init_end = std::uninitialized_fill_n(init_end, capacity2 - capacity_, invalid_index);
-        std::swap(capacity_, capacity2);
-        std::swap(locals_, locals2);
-        init_end = locals2 + capacity2;
+    void grow(std::size_t global_index) {
+        indices_.resize(global_index + 1, invalid_index);
     }
 
     BOOST_NOINLINE
-    void advance(std::size_t global, index_type& local) {
-        if (BOOST_UNLIKELY(next_ == invalid_index)) {
+    auto occupy(std::size_t global_index)
+    -> index_type
+    {
+        if (BOOST_UNLIKELY(next_index_ == invalid_index)) {
             throw std::range_error("too many indexed types");
         }
-        locals_[global] = local = next_++;
+        auto index = next_index_++;
+        indices_[global_index] = index;
+        return index;
     }
 };
+
+template <typename Domain, typename Index, typename Allocator>
+void swap(type_indexer<Domain, Index, Allocator>& a, type_indexer<Domain, Index, Allocator>& b)
+noexcept(noexcept(a.swap(b)))
+{
+    a.swap(b);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 }
