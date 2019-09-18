c8e5e3cb4b55e19377451c14133528ccd1da5c3b
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-09-18 19:31:44 +0300

    component buffer wip

--- include/nttx/detail/component_buffer.hpp
+++ include/nttx/detail/component_buffer.hpp
@@ -1,30 +1,33 @@
 #ifndef NTTX_DETAIL_INCLUDE_DETAIL_COMPONENT_BUFFER
 #define NTTX_DETAIL_INCLUDE_DETAIL_COMPONENT_BUFFER
 
+#include <nttx/detail/erased_component_type.hpp>
+
 #include <cstddef>
 
 namespace nttx::detail {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct component_buffer_view {
-    std::byte* bytes;
-    component_type const* type;
+    std::byte* bytes = nullptr;
+    erased_component_type const* type = nullptr;
 };
 
-template <typename Allocator = std::allocator<void>>
-struct component_buffer
-{
-    explicit
+struct component_buffer {
+    component_array_view *views = nullptr;
+    std::size_t size = 0;
+    std::size_t capacity = 0;
 
-private:
-    [[no_unique_address]]
-    allocator_type allocator_;
+    template <typename ProtoAllocator>
+    void initialize_with(std::span<erased_component_type const*> types,
+                         erased_component_type const* new_type,
+                         ProtoAllocator const& proto_allocator = ProtoAllocator())
+    {
+        auto allocator = rebind_allocator<component_buffer_view>(proto_allocator);
 
-    component_array_view *views;
-
-    std::size_t size;
-    std::size_t capacity;
+    }
 };
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 }
