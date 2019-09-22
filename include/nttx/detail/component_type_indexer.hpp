64eb29bb4ad7bd8f65bb9a97a8500cfe331912b2
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-09-22 15:49:34 +0300

    Move `component_type_index_domain` to the same header as `component_type_indexer`.

--- include/nttx/detail/component_type_indexer.hpp
+++ include/nttx/detail/component_type_indexer.hpp
@@ -2,10 +2,11 @@
 #define NTTX_DETAIL_INCLUDE_DETAIL_COMPONENT_TYPE_INDEXER
 
 #include <nttx/detail/type_indexer.hpp>
-#include <nttx/detail/component_type_index_domain.hpp>
 
 namespace nttx::detail {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct component_type_index_domain;
 
 using component_type_indexer = type_indexer<component_type_index_domain>;
 
